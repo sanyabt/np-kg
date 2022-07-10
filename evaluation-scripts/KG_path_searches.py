c1cca4faeabfd55cfece6184b190f44c57c7a29e
Sanya <sbt12@pitt.edu>
2022-07-10 14:55:49 -0400

    add path search scripts

---
+++ evaluation-scripts/KG_path_searches.py
@@ -0,0 +1,339 @@
+'''
+Author: Sanya B Taneja
+Date: 2021-09-29
+
+KG path search functions to generate and save paths:
+1. Single source shortest path: save_k_single_source_shortest_paths(G, source, k, filepath)
+2. Bidirectional shortest path: get_bidirectional_shortest_paths(G, source, target)
+3. k shortest paths: get_k_shortest_paths(G, source, target, k, weight='weight'), 
+4. k simple paths (nodes and edges), with and without cutoff: 
+	get_k_simple_paths(G, source, target, k, cutoff)
+	save_k_simple_paths(G, source, target, k, cutoff, filepath)
+5. print_graph_statistics(nx_graph): number of nodes, edges, average degree and density
+'''
+
+import os
+import os.path
+import networkx as nx
+import json
+import urllib
+import traceback
+from itertools import islice
+from rdflib import Graph, URIRef, BNode, Namespace, Literal
+from rdflib.namespace import RDF, OWL
+from tqdm import tqdm
+
+import pickle
+import pandas as pd
+import numpy as np
+#import pheknowlator kg_utils 
+import sys
+sys.path.append('../')
+from pkt_kg.utils import *
+
+combine_graph = True
+#change graph names and paths
+KG_PATH = '../resources/knowledge_graphs/'
+KG_NAME = 'PheKnowLator_machine_read_merged_instance_based_OWLNETS_v1.0.gpickle'
+
+NodeLabelsFile = '../resources/nodeLabels_20211021.pickle'
+DIR_OUT = '../output_files/'
+
+#define namespaces
+obo = Namespace('http://purl.obolibrary.org/obo/')
+napdi = Namespace('http://napdi.org/napdi_srs_imports:')
+
+node_dict = {
+	'EGCG': obo.CHEBI_4806,
+	'CATECHIN': obo.CHEBI_23053,
+	'GREENTEA': napdi.camellia_sinensis_leaf,
+	'KRATOM': napdi.mitragyna_speciosa,
+	'MITRAGYNINE': obo.CHEBI_6956,
+	'HYDROXY_MITRAGYNINE': napdi['7_hydroxy_mitragynine']
+}
+
+nodes_to_filter = [obo.CHEBI_24431, obo.CHEBI_25367, obo.SO_0000704, obo.PR_000029067, obo.PR_000000001, obo.GO_0008152, obo.SO_0000673,
+					URIRef('https://reactome.org/content/detail/R-HSA-1643685'), URIRef('https://reactome.org/content/detail/R-HSA-1430728')]
+
+#read nodeLabels dictionary
+with open(NodeLabelsFile, 'rb') as filep:
+	nodeLabels = pickle.load(filep)
+
+def save_k_single_source_shortest_paths(G, source, k, filepath):
+	paths = nx.single_source_shortest_path(G, source)
+	#if returned paths are dictionary
+	count = 0
+	file_save = open(filepath, 'w')
+	print('Saving path from {}'.format(str(source)))
+	for target, node_list in paths.items():
+		count += 1
+		if target != source:
+			if str(target) not in nodeLabels:
+				target_label = str(target).split('/')[-1]
+			else:
+				target_label = nodeLabels[str(target)]
+			file_save.write('\n{} - {} Path:\n'.format(str(source).split('/')[-1], target_label))
+			path_labels = get_path_labels(G, node_list)
+			for triples in path_labels:
+				for item in triples:
+					file_save.write(str(item)+' ')
+				file_save.write('\n')
+		if count == k:
+			break
+	file_save.close()
+
+'''
+print bidirectional shortest path between source and target
+and return length of shortest path
+'''
+def get_bidirectional_shortest_path(G, source, target, nodeLabels):
+	print('Searching for path from {} - {}'.format(str(source), str(target)))
+	pathx = nx.bidirectional_shortest_path(G, source, target)
+
+	path_labels = get_path_labels(G, pathx, nodeLabels)
+	path_uri = get_path_uri(G, pathx)
+	for triples in zip(path_labels, path_uri):
+		print(triples)
+	return path_labels, path_uri
+
+'''
+returns path labels and URIs for simple paths from source to target that have length greater than shortest path
+k = number of paths
+cutoff = maximum length of path
+shortestLens = length of shortest path between source and target
+'''
+def get_k_simple_paths(G, source, target, k, cutoff, shortestLens):
+	print('Searching for paths from {} - {}'.format(str(source), str(target)))
+	paths = nx.all_simple_edge_paths(G, source, target, cutoff=cutoff)
+	path_l = []
+	path_n = []
+	i = 0
+	while i<k:
+		try:
+			print('[info] applying next operator to search for a simple path of max length {}'.format(cutoff))
+			path = next(paths)
+		except StopIteration:
+			break
+		print('[info] Simple path found of length {}'.format(len(path))) 
+		if len(path) > shortestLens:
+			print('[info] Simple path length greater than shortest path length ({}) so adding to results'.format(shortestLens))
+			path_n.append(path)
+		i += 1
+
+	for newpath in path_n:
+		triple_list = []
+		for triple in newpath:
+			subj_lab = ''
+			pred_lab = ''
+			obj_lab = ''
+			subj = str(triple[0])
+			pred = str(triple[2])
+			obj = str(triple[1])
+			if subj in nodeLabels:
+				subj_lab = nodeLabels[subj]
+			if obj in nodeLabels:
+				obj_lab = nodeLabels[obj]
+			if pred in nodeLabels:
+				pred_lab = nodeLabels[pred]
+			triple_labels = (subj_lab, pred_lab, obj_lab)
+			triple_list.append(triple_labels)
+		path_l.append(triple_list)
+	return path_l, path_n
+
+'''
+returns path labels and URIs for simple paths from source to target that have length greater than shortest path
+skips paths that contain nodes from the filtered list defined above - []
+k = number of paths
+cutoff = maximum length of path
+shortestLens = length of shortest path between source and target
+There is a loop filter cutoff that decides how many paths to search without filtered nodes before moving on to the next.
+Currently filter_cutoff = 50
+
+'''
+def get_k_simple_paths_filtered(G, source, target, k, cutoff, shortestLens, nodes_to_filter):
+	
+	filter_cutoff = 50
+	print('Searching for filtered paths from {} - {}'.format(str(source), str(target)))
+	paths = nx.all_simple_edge_paths(G, source, target, cutoff=cutoff)
+	path_l = []
+	path_n = []
+	i = 0
+	filter_count = 0
+	while i<k:
+		try:
+			print('[info] applying next operator to search for a simple path of max length {}'.format(cutoff))
+			path = next(paths)
+		except StopIteration:
+			break
+		print('[info] Simple path found of length {}'.format(len(path))) 
+		if len(path) > shortestLens:
+			print('[info] Simple path length greater than shortest path length ({})'.format(shortestLens))
+			flag = True
+			triples = [item for sublist in path for item in sublist]
+			for node in triples:
+				if node in nodes_to_filter:
+					filter_count += 1
+					flag = False
+					break
+			if flag:
+				path_n.append(path)
+				i += 1
+			else:
+				print('Path contains filtered node, skipping path in results')
+				if filter_count >= filter_cutoff:
+					print('No path found without filtered nodes, moving to next iteration')
+					i += 1
+			
+	for newpath in path_n:
+		triple_list = []
+		for triple in newpath:
+			subj_lab = ''
+			pred_lab = ''
+			obj_lab = ''
+			subj = str(triple[0])
+			pred = str(triple[2])
+			obj = str(triple[1])
+			if subj in nodeLabels:
+				subj_lab = nodeLabels[subj]
+			if obj in nodeLabels:
+				obj_lab = nodeLabels[obj]
+			if pred in nodeLabels:
+				pred_lab = nodeLabels[pred]
+			triple_labels = (subj_lab, pred_lab, obj_lab)
+			triple_list.append(triple_labels)
+		path_l.append(triple_list)
+	return path_l, path_n
+
+'''
+returns list of k shortest paths from source to target
+'''
+def get_k_shortest_paths_filtered(G, source, target, k, weight='weight'):
+	print('Searching for shortest paths from {} - {}'.format(str(source), str(target)))
+	shortest_paths_filtered = []
+	i = 0
+	for path in paths:
+		for node in path:
+			if node in nodes_to_filter:
+				continue
+			else:
+				shortest_paths_filtered.append(path)
+	return shortest_paths_filtered
+
+'''
+returns list of k shortest paths from source to target but removes paths with nodes in filtered list above from results
+'''
+def get_k_shortest_paths(G, source, target, k, weight='weight'):
+	print('Searching for shortest paths from {} - {}'.format(str(source), str(target)))
+	paths = nx.all_shortest_paths(G, source, target, weight=weight)
+	return list(islice(nx.all_shortest_paths(G, source, target, weight=weight), k))
+
+def save_k_shortest_paths(G, source, target, k, filepath, weight='weight'):
+	paths = get_k_shortest_paths(G, source, target, k, weight='weight')
+	#print()
+	file_save = open(filepath, 'w')
+	source = str(source)
+	target = str(target)
+	source_label = source
+	target_label = target
+	if source in nodeLabels:
+		source_label = nodeLabels[source]
+	if target in nodeLabels:
+		target_label = nodeLabels[target]
+	file_save.write('\n{} - {} Shortest Path:\n'.format(source_label, target_label))
+	i = 0
+	for node_list in paths:
+		file_save.write('\nPATH: '+str(i)+'\n')
+		path_labels = get_path_labels(G, node_list, nodeLabels)
+		for triples in path_labels:
+			for item in triples:
+				file_save.write(str(item)+' ')
+			file_save.write('\n')
+		i += 1
+	file_save.close()
+
+def save_k_simple_paths(G, source, target, k, cutoff, filepath):
+	path_l, path_n = get_k_simple_paths(G, source, target, k, cutoff)
+	source = str(source)
+	target = str(target)
+	#print()
+	file_save = open(filepath, 'w')
+	
+	if source in nodeLabels:
+		source_label = nodeLabels[source]
+	if target in nodeLabels:
+		target_label = nodeLabels[target]
+	file_save.write('\n{} - {} Simple Path (cutoff= {} ):\n'.format(source_label, target_label, cutoff))
+	i = 0
+	for path_list in path_n:
+		file_save.write('\nPATH: '+str(i)+'\n')
+		for triples in path_list:
+			for item in triples:
+				file_save.write(str(item)+' ')
+			file_save.write('\n')
+		i += 1
+	file_save.close()
+
+def get_path_labels(nx_graph, path, nodeLabels):
+	path_labels = []
+	if len(path) < 1:
+		print('Path length 1, skipping')
+		return
+	for edge in zip(path, path[1:]):
+		data = nx_graph.get_edge_data(*edge)
+		pred = list(data.keys())[0]
+		node1_lab = str(edge[0])
+		node2_lab = str(edge[1])
+		if node1_lab in nodeLabels:
+			node1_lab = nodeLabels[node1_lab]
+		if node2_lab in nodeLabels:
+			node2_lab = nodeLabels[node2_lab]
+		pred_lab = nodeLabels[str(pred)]
+		if list(data.values())[0]:
+			if 'source_graph' in list(data.values())[0]:
+				source_graph = 'machine_read'
+			else:
+				source_graph = ''
+		else:
+			source_graph = ''
+		labels = [node1_lab, pred_lab, node2_lab, source_graph]
+		path_labels.append(labels)
+	return path_labels
+
+def get_path_uri(nx_graph, path):
+	path_uri = []
+	if len(path) < 1:
+		print('Path length 1, skipping')
+		return
+	for edge in zip(path, path[1:]):
+		data = nx_graph.get_edge_data(*edge)
+		pred = list(data.keys())[0]
+		attribute = list(data.values())
+		uri = [str(edge[0]), str(pred), str(edge[1]), attribute]
+		path_uri.append(uri)
+	return path_uri
+
+'''
+prints common statistics for the graph passed to function including number of nodes, edges
+average degree, node density and nodes with the highest degree
+'''
+def print_graph_statistics(nx_graph):
+	# get the number of nodes, edges, and self-loops
+	nodes = nx.number_of_nodes(nx_graph)
+	edges = nx.number_of_edges(nx_graph)
+	self_loops = nx.number_of_selfloops(nx_graph)
+
+	print('There are {} nodes, {} edges, and {} self-loop(s)'.format(nodes, edges, self_loops))
+	# get degree information
+	avg_degree = float(edges) / nodes
+
+	print('The Average Degree is {}'.format(avg_degree))
+	# get 5 nodes with the highest degress
+	n_deg = sorted([(str(x[0]), x[1]) for x in  nx_graph.degree], key=lambda x: x[1], reverse=1)[:6]
+
+	for x in n_deg:
+		print('{} (degree={})'.format(x[0], x[1]))
+	# get network density
+	density = nx.density(nx_graph)
+
+	print('The density of the graph is: {}'.format(density))
+
---
+++ evaluation-scripts/pathSearchMain.py
@@ -0,0 +1,298 @@
+import os.path
+import networkx as nx
+import json
+import urllib
+import traceback
+from itertools import islice
+from rdflib import Graph, URIRef, BNode, Namespace, Literal
+from rdflib.namespace import RDF, OWL
+from tqdm import tqdm
+
+import pickle
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append('../')
+from pkt_kg.utils import *
+import KG_path_searches
+
+combine_graph = False
+#change graph names and paths
+KG_PATH = '../resources/knowledge_graphs/'
+KG_NAME_MERGED = 'PheKnowLator_machine_read_merged_instance_based_OWLNETS_v1.0.gpickle'
+NodeLabelsFile = '../resources/nodeLabels_v1.0.pickle'
+DIR_OUT = '../output_files/'
+
+#set to false for no filtering
+filter_nodes = True
+time_slicing = True
+MAXNUMPATHS = 10
+
+#define namespaces
+obo = Namespace('http://purl.obolibrary.org/obo/')
+napdi = Namespace('http://napdi.org/napdi_srs_imports:')
+
+#read nodeLabels dictionary
+with open(NodeLabelsFile, 'rb') as filep:
+	nodeLabels = pickle.load(filep)
+
+node_dict = {
+	'EGCG': obo.CHEBI_4806,
+	'ECG': obo.CHEBI_70255,
+	'EPICATECHIN': obo.CHEBI_90,
+	'CATECHIN': obo.CHEBI_23053,
+	'GREENTEA': napdi.camellia_sinensis_leaf,
+	'KRATOM': napdi.mitragyna_speciosa,
+	'MITRAGYNINE': obo.CHEBI_6956,
+	'HYDROXY_MITRAGYNINE': napdi['7_hydroxy_mitragynine'],
+	'CYP3A4': obo.PR_P08684,
+	'CYP2D6': obo.PR_P10635,
+	'CYP2C19': obo.PR_P33261,
+	'UGT1A1_protein': obo.PR_P22309,
+	'UGT1A8_protein': obo.PR_Q9HAW9,
+	'UGT1A10_protein': obo.PR_Q9HAW8,
+	'buspirone': obo.CHEBI_3223,
+	'nadolol': obo.CHEBI_7444,
+	'raloxifene': obo.CHEBI_8772,
+	'midazolam': obo.CHEBI_6931,
+	'dextromethorphan': obo.CHEBI_4470,
+	'seizure': obo.HP_0001250
+}
+
+greentea_tuples = [
+	(node_dict['GREENTEA'], node_dict['CYP3A4']),
+	(node_dict['GREENTEA'], node_dict['CYP2D6']),
+	(node_dict['GREENTEA'], node_dict['UGT1A1_protein']),
+	(node_dict['GREENTEA'], node_dict['UGT1A8_protein']),
+	(node_dict['GREENTEA'], node_dict['UGT1A10_protein']),
+	(node_dict['EGCG'], node_dict['CYP3A4']),
+	(node_dict['EGCG'], node_dict['CYP2C19']),
+	(node_dict['EGCG'], node_dict['CYP2D6']),
+	(node_dict['EGCG'], node_dict['UGT1A1_protein']),
+	(node_dict['EGCG'], node_dict['UGT1A8_protein']),
+	(node_dict['EGCG'], node_dict['UGT1A10_protein']),
+	(node_dict['ECG'], node_dict['CYP3A4']),
+	(node_dict['ECG'], node_dict['UGT1A1_protein']),
+	(node_dict['ECG'], node_dict['UGT1A8_protein']),
+	(node_dict['ECG'], node_dict['UGT1A10_protein']),
+	(node_dict['CATECHIN'], node_dict['UGT1A1_protein']),
+	(node_dict['CATECHIN'], node_dict['UGT1A8_protein']),
+	(node_dict['CATECHIN'], node_dict['UGT1A10_protein']),
+	(node_dict['EPICATECHIN'], node_dict['UGT1A1_protein']),
+	(node_dict['EPICATECHIN'], node_dict['UGT1A8_protein']),
+	(node_dict['EPICATECHIN'], node_dict['UGT1A10_protein']),
+	(node_dict['GREENTEA'], node_dict['raloxifene']),
+	(node_dict['GREENTEA'], node_dict['nadolol']),
+	(node_dict['GREENTEA'], node_dict['buspirone'])
+]
+
+kratom_tuples = [
+	(node_dict['KRATOM'], node_dict['CYP2D6']),
+	(node_dict['KRATOM'], node_dict['CYP3A4']),
+	(node_dict['MITRAGYNINE'], node_dict['CYP3A4']),
+	(node_dict['MITRAGYNINE'], node_dict['CYP2D6']),
+	(node_dict['MITRAGYNINE'], node_dict['CYP2C19']),
+	(node_dict['HYDROXY_MITRAGYNINE'], node_dict['UGT1A1_protein']),
+	(node_dict['HYDROXY_MITRAGYNINE'], node_dict['CYP2C19']),
+	(node_dict['KRATOM'], node_dict['midazolam']),
+	(node_dict['KRATOM'], node_dict['dextromethorphan']),
+	(node_dict['KRATOM'], node_dict['seizure'])
+]
+
+nodes_to_filter = [obo.CHEBI_24431, obo.CHEBI_25367, obo.SO_0000704, obo.PR_000029067, obo.PR_000000001, obo.GO_0008152, obo.SO_0000673,
+					URIRef('https://reactome.org/content/detail/R-HSA-1643685'), URIRef('https://reactome.org/content/detail/R-HSA-1430728')]
+
+
+def get_all_paths(nx_graph, tuples):
+	
+	df = pd.DataFrame(columns = ['path_type','path_start', 'path_start_label', 'path_end', 'path_end_label', 'path_count','path_step','subject_label','predicate_label',
+							'object_label','subject_uri','predicate_uri','object_uri','source_data', 'pub_year', 'pmid'])
+	shortestLens = 0
+	for tpl in tuples:
+		(s,o) = tpl
+		startNd = s
+		endNd = o
+		try:
+			shortestPathLabels, shortestPathUri = KG_path_searches.get_bidirectional_shortest_path(nx_graph, startNd, endNd, nodeLabels)
+			shortestLens = len(shortestPathLabels)
+		except:
+			print('Could not find bidirectional shortest path between {} and {}'.format(startNd, endNd))
+			shortestLens = 2
+		
+		if shortestLens == 0:
+			print('Shortest path between {} and {} not found. Setting shortest path length=2.'.format(startNd, endNd))
+			shortestLens = 2
+
+		stepCount = 0
+
+		for triples in zip(shortestPathLabels, shortestPathUri):
+							
+				attribute_dict = triples[1][3][0]
+
+				if 'source_graph' in attribute_dict:
+					source_data = 'machine_read'
+				else:
+					source_data = ''
+				if 'timestamp' in attribute_dict:
+					pub_year = attribute_dict['timestamp']
+				else:
+					pub_year = ''
+				if 'pmid' in attribute_dict:
+					pmid = attribute_dict['pmid']
+				else:
+					pmid = ''
+
+				df_temp = {
+					'path_type': 'bi_shortest',
+					'path_start': str(startNd),
+					'path_start_label': nodeLabels[str(startNd)],
+					'path_end': str(endNd),
+					'path_end_label': nodeLabels[str(endNd)],
+					'path_count': 1,
+					'path_step': stepCount,
+					'path_length': len(shortestPathLabels),
+					'subject_label': triples[0][0],
+					'predicate_label': triples[0][1],
+					'object_label': triples[0][2],
+					'subject_uri': str(triples[1][0]),
+					'predicate_uri': str(triples[1][1]),
+					'object_uri': str(triples[1][2]),
+					'source_data': source_data,
+					'pub_year': pub_year,
+					'pmid': pmid
+				}
+
+				df = df.append(df_temp, ignore_index=True)
+				stepCount += 1
+
+		#path_s = KG_path_searches.get_k_shortest_paths(nx_graph, startNd, endNd, MAXNUMPATHS)
+		MAXPATHLENGTH = shortestLens + 10
+		
+		#shortestCount = 0
+		
+		#save shortest path as dictionary and add to dataframe -- running longer than few hours so commented out for now
+		'''for node_list in path_s:
+			stepCount = 0
+			path_labels = KG_path_searches.get_path_labels(nx_graph, node_list, nodeLabels)
+			path_uri = KG_path_searches.get_path_uri(nx_graph, node_list)
+			for triples in zip(path_labels, path_uri):
+				
+				if 'timestamp' in triples[1][3][0]:
+					pub_year = triples[1][3][0]['timestamp']
+				else:
+					pub_year = ''
+
+				df_temp = {
+					'path_type': 'shortest',
+					'path_start': str(startNd),
+					'path_start_label': nodeLabels[str(startNd)],
+					'path_end': str(endNd),
+					'path_end_label': nodeLabels[str(endNd)],
+					'path_count': shortestCount,
+					'path_step': stepCount,
+					'path_length': len(path_labels),
+					'subject_label': triples[0][0],
+					'predicate_label': triples[0][1],
+					'object_label': triples[0][2],
+					'subject_uri': triples[1][0],
+					'predicate_uri': triples[1][1],
+					'object_uri': triples[1][2],
+					'source_data': triples[0][3],
+					'pub_year': pub_year,
+					'pmid': pmid
+				}
+				df.append(df_temp, ignore_index=True)
+				stepCount += 1
+			
+			shortestCount += 1
+'''
+		pathCount = 0
+		#get simple paths and add to dataframe
+		for i in range(shortestLens, MAXPATHLENGTH+1):
+			cutoff = i
+			
+			if filter_nodes:
+				path_l, path_n = KG_path_searches.get_k_simple_paths_filtered(nx_graph, startNd, endNd, MAXNUMPATHS, cutoff,
+																			shortestLens, nodes_to_filter)
+			else:
+				path_l, path_n = KG_path_searches.get_k_simple_paths(nx_graph, startNd, endNd, 
+																		MAXNUMPATHS, cutoff, shortestLens)
+			#create dataframe from path results and append to main dataframe at end of loop
+			for item in zip(path_l, path_n):
+
+				pathStep = 0
+				for triples in zip(item[0], item[1]):
+
+					s = triples[1][0]
+					p = triples[1][2]
+					o = triples[1][1]
+					
+					attribute_dict = nx_graph[s][o][p]
+
+					if 'source_graph' in attribute_dict:
+						source_data = 'machine_read'
+					else:
+						source_data = ''
+					if 'timestamp' in attribute_dict:
+						pub_year = attribute_dict['timestamp']
+					else:
+						pub_year = ''
+					if 'pmid' in attribute_dict:
+						pmid = attribute_dict['pmid']
+					else:
+						pmid = ''
+
+					df_temp = {
+						'path_type': 'simple',
+						'path_start': str(startNd),
+						'path_start_label': nodeLabels[str(startNd)],
+						'path_end': str(endNd),
+						'path_end_label': nodeLabels[str(endNd)],
+						'path_count': pathCount,
+						'path_step': pathStep,
+						'path_length': len(item[0]),
+						'subject_label': triples[0][0],
+						'predicate_label': triples[0][1],
+						'object_label': triples[0][2],
+						'subject_uri': str(triples[1][0]),
+						'predicate_uri': str(triples[1][2]),
+						'object_uri': str(triples[1][1]),
+						'source_data': source_data,
+						'pub_year': pub_year,
+						'pmid': pmid
+					}
+
+					df = df.append(df_temp, ignore_index=True)
+					pathStep += 1
+				pathCount += 1
+	return df
+
+if __name__ == '__main__':
+
+	##READ MERGED GRAPH (PL + REACH + SEMREP + INFERRED)
+	nx_graph = nx.read_gpickle(KG_PATH+KG_NAME_MERGED)
+
+	df_gt = get_all_paths(nx_graph, greentea_tuples)
+	df_gt = df_gt.fillna('')
+	
+	if filter_nodes:
+		outfilegt = outfilegt_filtered
+
+	df_gt.to_csv(outfilegt, sep='\t', index=False)
+
+	df_kt = get_all_paths(nx_graph, kratom_tuples)
+	df_kt = df_kt.fillna('')
+
+	if filter_nodes:
+		outfilekt = outfilekt_filtered
+	df_kt.to_csv(outfilekt, sep='\t', index=False)
+
+
+
+
+
+
+
+
+
+
